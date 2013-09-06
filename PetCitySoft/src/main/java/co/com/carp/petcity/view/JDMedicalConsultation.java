7f1b20a78c64210e04e699848c44478df656ee6f
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2013-09-05 23:01:17 -0500

    New Screen to show consultation detail

---
+++ PetCitySoft/src/main/java/co/com/carp/petcity/view/JDMedicalConsultation.java
@@ -0,0 +1,18 @@
+package co.com.carp.petcity.view;
+
+import javax.swing.JDialog;
+
+/**
+ * This class is attempt to display the information about a consultation.
+ * 
+ * @author Carlos Rodriguez
+ *
+ */
+public class JDMedicalConsultation extends JDialog {
+
+	/**
+	 * Generated serial version
+	 */
+	private static final long serialVersionUID = 1874606033331121292L;
+
+}
